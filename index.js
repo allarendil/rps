294768cc91964ebbf95e7624ffb6d6f095036b5b
allarendil <tebicat516@gmail.com>
2023-08-31 10:57:27 -0400

    Fix loop to prompt each round

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let score = 0;
+
 function getComputerChoice() {
     function random(arr) {
         return arr[Math.floor(Math.random() * arr.length)]
@@ -9,62 +11,71 @@ function getComputerChoice() {
     return random(rpsArray);
 }
 
-function playRound(playerSelection, computerSelection) {
 
-    let newChoice = playerSelection.toLowerCase();
-
-    if (newChoice === computerSelection) {
-        return "Tie! Play Again!";
-    } else if (
-        (newChoice == "rock" && computerSelection == "paper") ||
-        (newChoice == "paper" && computerSelection == "scissors") ||
-        (newChoice == "scissors" && computerSelection == "rock")
-    ) { return "No Point";
-    } else if (
-        (newChoice == "rock" && computerSelection == "scissors") ||
-        (newChoice == "paper" && computerSelection == "rock") ||
-        (newChoice == "scissors" && computerSelection == "paper")
-    ) {
-        return "Yay You got a point!";
-    };
-};
 
 
 function game() {
 
-    let score = 0;
+    for (let i = 0; i < 5; i++) {
 
     const playerSelection = prompt("Rock, Paper, or Scissors?", '');
 
     const computerSelection = getComputerChoice();
-    
 
-    for (let i = 0; i < 5; i++) {
-
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
+    playRound(playerSelection, computerSelection);
 
     console.log(computerSelection);
     console.log(playerSelection);
+
+    keepScore();
+    console.log(score);
+
+    onWin();
+
     
+    function playRound(playerSelection, computerSelection) {
+
+        let newChoice = playerSelection.toLowerCase();
+    
+        if (newChoice === computerSelection) {
+            return "Tie! Play Again!";
+        } else if (
+            (newChoice == "rock" && computerSelection == "paper") ||
+            (newChoice == "paper" && computerSelection == "scissors") ||
+            (newChoice == "scissors" && computerSelection == "rock")
+        ) { return "No Point";
+        } else if (
+            (newChoice == "rock" && computerSelection == "scissors") ||
+            (newChoice == "paper" && computerSelection == "rock") ||
+            (newChoice == "scissors" && computerSelection == "paper")
+        ) {
+            return "Yay You got a point!";
+        };
+    };
+    
+   
+    function keepScore() {
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
     if (result == "Yay You got a point!") {
         score++
     } else {
         return "Try Again"
     };
 
-   
     console.log(score);
 };
 
-    console.log(score);
-
+    function onWin() {
 if (score >= 3) {
-    return "You win!"
+    console.log("You win!")
 } else {
-    return "You Lose ;("
+    console.log("You Lose ;(")
 };
 
+};
+    };
 };
 
 
