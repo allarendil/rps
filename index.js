c0dd40815410d19fe302d49300c0384ee4128be1
allarendil <tebicat516@gmail.com>
2023-10-04 13:25:12 -0400

    Got buttons and text box to work

--- index.js
+++ index.js
@@ -13,87 +13,92 @@ function getComputerChoice() {
     return random(rpsArray);
 };
 
-
-function game() {
-
-for (let i = 0; i < 5; i++) {
-
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-button.addEventListener('click', function(event) {
-
-//FIGURE OUT HOW TO PULL BUTTON ID AND USE IT FOR PLAYER SLECTION
-
-    return button.id;
-})
-});
-
-const playerSelection = button.id; // CHANGE THIS ^
-const computerSelection = getComputerChoice();
-
-playRound(playerSelection, computerSelection);
-
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(score);
-
-keepScore();
-    console.log(score);
-
-onWin();
-
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "Tie! Play Again!";
+    if (playerSelection === computerSelection) {
+        const p = document.createElement('p')
+        p.innerText = "Tie! Play Again!";
+        textBox.appendChild(p);
     } else if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock")
-    ) { return "No Point";
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+    ) { 
+        const p = document.createElement('p')
+        p.innerText = "No Point";
+        textBox.appendChild(p);
     } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return "Yay You got a point!";
+        score++
+        const p = document.createElement('p')
+        p.innerText = "Yay You got a point!";
+        textBox.appendChild(p);
     };
 };
             
     
-function keepScore() {
-    let result = playRound();
-    console.log(result);
-    if (result == "Yay You got a point!") {
-        score++
-    } else {
-        return "Try Again"
-    };
-    console.log(score);
-};
+//function keepScore() {
+//    let result = playRound();
+//    console.log(result);
+//    if (result == "Yay You got a point!") {
+//        score++
+//    } else {
+//        return "Try Again"
+//    };
+//    console.log(score);
+//};
 
 function onWin() {
     if (score >= 3) {
     console.log("You win!")
+    const h2 = document.createElement('h2')
+        h2.innerText = "Best 3 of 5, You win!!";
+        textBox.appendChild(h2);
 };
-};
+}
 
 
+//function game() {
+
+//for (let i = 0; i < 5; i++) {
 
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+button.addEventListener('click', () => {
+
+const playerSelection = button.id;
+const computerSelection = getComputerChoice(); 
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+playRound(playerSelection, computerSelection);
+
+//keepScore();
+    console.log(score);
+
+onWin();
 
 const para = document.createElement('h3');
 para.textContent = score;
 para.style.color = 'blue';
 textBox.appendChild(para);
 
-};
-};
+})
+});
 
 
 
-game();
+
+//};
+//};
+
+
+
+//game();
 
 
 
